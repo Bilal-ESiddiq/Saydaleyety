cdb632c7cb840917ba709e5e390f79dcd74db3f2
Owner <Owner@Dell>
2018-02-28 22:54:03 +0200

    Homepage

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 7878);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -22,7 +22,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+//development only
 if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
